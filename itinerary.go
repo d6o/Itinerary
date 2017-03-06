c9f57dab10e75b2e52cf76ea31c39c7516bbd00d
Diego Siqueira <diego9889@gmail.com>
2017-03-05 23:36:16 -0300

    Small fixes
    Signed-off-by: DiSiqueira <dieg0@live.com>

--- itinerary.go
+++ itinerary.go
@@ -2,8 +2,6 @@ package itinerary
 
 import (
 	"net/http"
-	"net/url"
-	"strconv"
 	"strings"
 )
 
@@ -31,13 +29,10 @@ func (r *Router) NewPath(f func(http.ResponseWriter, *http.Request)) IPath {
 
 // ServeHTTP find the right path and calls it handler.
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	r.routeToQuery(req)
 	handler := r.Match(req)
-
 	if handler == nil {
 		handler = http.NotFoundHandler()
 	}
-
 	handler.ServeHTTP(w, req)
 }
 
@@ -59,16 +54,8 @@ func (r *Router) Match(req *http.Request) http.Handler {
 	return nil
 }
 
-// routeToQuery makes the routes parts accessible trough Query String
-func (r *Router) routeToQuery(req *http.Request) {
-	req.URL.Path = strings.Trim(req.URL.Path, "/")
-	if req.URL.Path == "" {
-		return
-	}
-	parts := strings.Split(req.URL.Path, "/")
-	value := make(url.Values)
-	for i, part := range parts {
-		value.Set(r.Prefix+strconv.Itoa(i), part)
-	}
-	req.URL.RawQuery = url.Values(value).Encode() + "&" + req.URL.RawQuery
+// RouteToQuery makes the routes parts accessible trough Query String
+func RouteToQuery(req *http.Request) {
+	replace := "&itinerary="
+	req.URL.RawQuery = req.URL.RawQuery + strings.Replace(strings.TrimRight(req.URL.Path, "/"), "/", replace, -1)
 }
